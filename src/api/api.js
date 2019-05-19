981761b2175f7efce39028849422d91bf2802ab2
craigweighman <craigweighman@gmail.com>
2019-05-19 20:52:34 +0100

    pull participants from API on mount

--- src/api/api.js
+++ src/api/api.js
@@ -4,12 +4,51 @@ const request = axios.create({
   baseURL: "https://api.justgiving.com"
 });
 
-export const getFundraisingPageDetailsById = async pageId => {
-  const fundraisingPage = await request.get(
-    `/${justGivingAPI}/v1/fundraising/pagebyid/${pageId}`
-  );
-  return fundraisingPage;
+export const getParticipantsById = async participantId => {
+  const participants = await axios.all([
+    request.get(
+      `/${justGivingAPI}/v1/fundraising/pagebyid/${participantId[0]}`
+    ),
+    request.get(
+      `/${justGivingAPI}/v1/fundraising/pagebyid/${participantId[1]}`
+    ),
+    request.get(
+      `/${justGivingAPI}/v1/fundraising/pagebyid/${participantId[2]}`
+    ),
+    request.get(
+      `/${justGivingAPI}/v1/fundraising/pagebyid/${participantId[3]}`
+    ),
+    request.get(`/${justGivingAPI}/v1/fundraising/pagebyid/${participantId[4]}`)
+  ]);
+  return participants;
 };
 
-const getRandomInteger = (max, min) =>
+export const getRandomInteger = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
+
+export const buildParticipantArray = data => {
+  return data.map(participant => participant["data"]);
+};
+
+export const pageId = [
+  2546738,
+  2546740,
+  2546765,
+  2646765,
+  2746765,
+  2846765,
+  2856765,
+  2876765,
+  2886765,
+  2887765,
+  2889765,
+  2889465,
+  2889265,
+  2889165,
+  2889115,
+  2889112,
+  2689112,
+  2589112,
+  2079112,
+  2059112
+];
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Router } from "@reach/router";
+import * as api from "../api/api.js";
 import ParticipantList from "../components/ParticipantList.js";
 import Donate from "../components/Donate.js";
 import Charity from "../components/Charity.js";
@@ -7,20 +8,45 @@ import "../style/Main.css";
 
 class Main extends Component {
   state = {
+    isLoading: true,
+    isError: false,
     participants: []
   };
 
   render() {
+    const { participants } = this.state;
+    console.log(participants);
+
     return (
       <div className="Main">
         <Router>
-          <ParticipantList path="/" />
-          <Donate path="/:participant_id" />
-          <Charity path="/:charity_id" />
+          <ParticipantList path="/" participants={participants} />
+          <Donate
+            path="/participant/:participant_id"
+            participants={participants}
+          />
+          <Charity path="/charity/:charity_id" participants={participants} />
         </Router>
       </div>
     );
   }
+
+  componentDidMount = props => {
+    this.fetchParticipantsById();
+    this.setState({ isLoading: false });
+  };
+
+  fetchParticipantsById = () => {
+    const slicedArray = api.getRandomInteger(0, 14);
+    const participantIdArray = api.pageId.slice(slicedArray, slicedArray + 5);
+    api
+      .getParticipantsById(participantIdArray)
+      .then(data => {
+        const participants = api.buildParticipantArray(data);
+        this.setState({ participants });
+      })
+      .catch(error => this.setState({ isError: error.response.data }));
+  };
 }
 
 export default Main;
