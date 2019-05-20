8cc9f67cd174f457096543e913701056f7a13c24
craigweighman <craigweighman@gmail.com>
2019-05-20 13:22:22 +0100

    header links to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "@reach/router";
 import logo from "../assets/header-logo.png";
 import "../style/Header.css";
 
 const Header = () => {
   return (
-    <div className="Header">
-      <img src={logo} alt="london-marathon-logo" className="header-logo" />
-    </div>
+    <Link to={`/`}>
+      <div className="Header">
+        <img src={logo} alt="london-marathon-logo" className="header-logo" />
+      </div>
+    </Link>
   );
 };
 
